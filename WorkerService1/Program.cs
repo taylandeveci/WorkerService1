726fd17d05a94c02164a590dc5e12d8190bb3d65
taylandeveci <devecitaylan@gmail.com>
2025-07-09 14:37:58 +0300

    update

--- WorkerService1/Program.cs
+++ WorkerService1/Program.cs
@@ -1,36 +1,7 @@
-public class Worker : BackgroundService
-{
-    private readonly ILogger<Worker> _logger;
-    private readonly HttpClient _httpClient;
+using WorkerService1;
 
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-        _httpClient = new HttpClient(new HttpClientHandler
-        {
-            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-        });
-    }
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-
-            try
-            {
-                var response = await _httpClient.GetAsync("https://localhost:7295/api/pokemon", stoppingToken);
-                var data = await response.Content.ReadAsStringAsync();
-
-                _logger.LogInformation("Pokemon verisi alýndý: {data}", data);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "API çaðrýsý sýrasýnda hata oluþtu.");
-            }
-
-            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
-        }
-    }
-}
+var host = builder.Build();
+host.Run();
